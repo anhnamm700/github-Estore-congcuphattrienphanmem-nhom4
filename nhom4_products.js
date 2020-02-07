5a1d3a58f7541ff806896ed765ac6dae0cbaff08
anhnamm700 <60686832+anhnamm700@users.noreply.github.com>
2020-02-07 10:26:57 +0700

    .

--- nhom4_products.js
+++ nhom4_products.js
@@ -152,63 +152,131 @@ function proSale() {
  proSale();     
 
 
-//  var laptopTab = [
-//      {
-//          img: 'https://cdn.tgdd.vn/Products/Images/44/106875/apple-macbook-air-mqd32sa-a-i5-5350u-400x400.jpg',
-//          name: 'Macbook Air 2017 128GB',
-//          cost: '19.990.000đ'
-//      },
-
-//      {
-//         img: 'https://cdn.tgdd.vn/Products/Images/44/210665/acer-aspire-a315-34-c2h9-n4000-4gb-256gb-win10-nx9-1-400x400.jpg',
-//         name: 'Macbook Air 2017 128GB',
-//         cost: '19.990.000đ'
-//     },
-
-//     {
-//         img: 'https://cdn.tgdd.vn/Products/Images/44/106875/apple-macbook-air-mqd32sa-a-i5-5350u-400x400.jpg',
-//         name: 'Macbook Air 2017 128GB',
-//         cost: '19.990.000đ'
-//     },
-
-//     {
-//         img: 'https://cdn.tgdd.vn/Products/Images/44/106875/apple-macbook-air-mqd32sa-a-i5-5350u-400x400.jpg',
-//         name: 'Macbook Air 2017 128GB',
-//         cost: '19.990.000đ'
-//     },
-
-//     {
-//         img: 'https://cdn.tgdd.vn/Products/Images/44/106875/apple-macbook-air-mqd32sa-a-i5-5350u-400x400.jpg',
-//         name: 'Macbook Air 2017 128GB',
-//         cost: '19.990.000đ'
-//     },
-
-//     {
-//         img: 'https://cdn.tgdd.vn/Products/Images/44/106875/apple-macbook-air-mqd32sa-a-i5-5350u-400x400.jpg',
-//         name: 'Macbook Air 2017 128GB',
-//         cost: '19.990.000đ'
-//     },
-//  ];
+ 
 
 
-//  function laptopTabPro() {
-//     var html2 = '<div class="products-list">';
-//     html2 += '<h3 class="slogen">laptop & tablet</h3>';
-//     html2 += '<div class="products-menu">';
-//     for(var i = 0; i < laptopTabPro.length; i++) {
-//         var productSal = laptopTabPro[i];
-//         html2 += '<div class="product">';
-//         html2 +=     '<img class="product-img" src="' + laptopTabPro.img + '"/>';
-//         html2 +=     '<h6 class="product-name">' + laptopTabPro.name + '</h6>';
-//         html2 +=     '<span class="product-cost">' + laptopTabPro.cost + '</span>';
-//         html2 +=     '<span class="notBuy">' + laptopTabPro.notbuy + '</span>';
-//         html2 +=     '<button class="cartBuy">Thêm vào giỏ</button>';
-//         html2 += '</div>';
-//     }
-//     html2 += '</div>';
-//     html2 += '</div>';
-   
-//     document.getElementById('productsMenu2').innerHTML = html2;
 
-// }
-// laptopTabPro();
+
+
+//SMENU
+
+
+
+var smenu_Iphone = [
+    {
+        name: 'Iphone 5/5s'
+    },
+    {
+        name: 'Iphone 6/6s/6s Plus'
+    },
+    {
+        name: 'Iphone 7/7 Plus'
+    },
+    {
+        name: 'Iphone 8/8 Plus'
+    },
+    {
+        name: 'Iphone X/XS/XS Max'
+    },
+    {
+        name: 'Iphone 11/11 Pro/11 Pro Max'
+    },
+];
+
+var smenu_Ss = [
+    {
+        name: 'Samsung Note Series'
+    },
+    {
+        name: 'Samsung S Series'
+    },
+    {
+        name: 'Samsung A Series'
+    },
+];
+
+var menuDisplayIp = document.getElementsByClassName('nav-second')[0];
+var menuDisplaySs = document.getElementsByClassName('nav-second')[1];
+var menuDisplayOp = document.getElementsByClassName('nav-second')[2];
+var menuDisplayXi = document.getElementsByClassName('nav-second')[3];
+
+function smenuDisplayIp() {
+    var html = '';
+    for(var i = 0; i < smenu_Iphone.length; i++) {
+        html += '<span class="nav__item">' + '<a href="#" class="nav__item__item">' + smenu_Iphone[i].name + '</a>' + '</span>';
+    }
+
+    menuDisplayIp.innerHTML = html;
+
+}
+
+
+smenuDisplayIp();
+
+
+function smenuDisplaySs() {
+    var html = '';
+    for(var i = 0; i < smenu_Ss.length; i++) {
+        html += '<span class="nav__item">' + '<a href="#" class="nav__item__item">' + smenu_Ss[i].name + '</a>' + '</span>';
+    }
+    menuDisplaySs.innerHTML = html;
+}
+smenuDisplaySs();
+
+
+
+var smenu_Op = [
+    {
+        name: 'Oppo N Series'
+    },
+    {
+        name: 'Oppo R Series'
+    },
+    {
+        name: 'Oppo F Series'
+    },
+    {
+        name: 'Oppo A Series'
+    },
+    {
+        name: 'Oppo Find Series'
+    },
+];
+
+
+function smenuDisplayOp() {
+    var html = '';
+    for(var i = 0; i < smenu_Ss.length; i++) {
+        html += '<span class="nav__item">' + '<a href="#" class="nav__item__item">' + smenu_Op[i].name + '</a>' + '</span>';
+    }
+    menuDisplayOp.innerHTML = html;
+}
+smenuDisplayOp();
+
+
+var smenu_Xi = [
+    {
+        name: 'Xiaomi Mi Mix Series'
+    },
+    {
+        name: 'Xiaomi Mi Note Series'
+    },
+    {
+        name: 'Xiaomi Mi Series'
+    },
+    {
+        name: 'Xiaomi Redmi Note Series'
+    },
+    {
+        name: 'Xiaomi Redmi Series'
+    },
+];
+
+function smenuDisplayXi() {
+    var html = '';
+    for(var i = 0; i < smenu_Ss.length; i++) {
+        html += '<span class="nav__item">' + '<a href="#" class="nav__item__item">' + smenu_Xi[i].name + '</a>' + '</span>';
+    }
+    menuDisplayXi.innerHTML = html;
+}
+smenuDisplayXi();
